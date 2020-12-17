10b56978661eab0762ba78ff0c1fcb27b23b0048
root <asperanza93@github.com>
2020-12-17 09:59:34 -0500

    table_stats.sql - more cols

--- sql/table_stats.sql
+++ sql/table_stats.sql
@@ -3,5 +3,10 @@ SELECT CONCAT(table_schema, '.', table_name) as 'TABLE',
        CONCAT(ROUND(data_length / ( 1024 * 1024 * 1024 ), 2), 'G') DATA, 
        CONCAT(ROUND(index_length / ( 1024 * 1024 * 1024 ), 2), 'G') IDX, 
        CONCAT(ROUND(( data_length + index_length ) / ( 1024 * 1024 * 1024 ), 2), 'G') 'TOTAL SIZE', 
-       ROUND(index_length / data_length, 2)  Idx_frag_pct, CONCAT(ROUND(( data_free / 1024 / 1024),2), 'MB') AS data_free 
-       FROM information_schema.TABLES  WHERE table_name='sbtest1';
+       ROUND(index_length / data_length, 2)  Idx_frag_pct, CONCAT(ROUND(( data_free / 1024 / 1024),2), 'MB') AS data_free,
+create_time as create_timeo,
+update_time as upd_timeo,
+check_time as last_checked
+       FROM information_schema.TABLES  
+
+rder by data_length+index_length desc limit 100;
